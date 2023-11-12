51c0368e4992acbcf86609f580ad86b1c5564aed
Breno Azevedo <maddox.br@gmail.com>
2023-11-12 20:28:31 -0300

    * Fixed raptor artillery textures

--- objects3d/Units/Other/raptorartillery.dae.lua
+++ objects3d/Units/Other/raptorartillery.dae.lua
@@ -1,8 +1,8 @@
 model = {
       radius = 4.65,
       height = 4.25,
-      tex1 = "raptorartillery_1.bmp",
-      tex2 = "raptorartillery_2.bmp", --"raptorartillery_2.bmp",
+      tex1 = "raptorartillery1.png",
+      tex2 = "raptorartillery_2.tga", --"raptorartillery_2.bmp",
       --midpos = {0,0,0},
       --rotAxisSigns = {-1, -1, -1}
 }
